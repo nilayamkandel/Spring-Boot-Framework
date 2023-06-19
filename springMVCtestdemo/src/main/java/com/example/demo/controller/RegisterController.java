1376ce9c16f4a82bbc01def64769bc56f12fe9b4
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-19 21:56:41 +0545

    register controller

--- springMVCtestdemo/src/main/java/com/example/demo/controller/RegisterController.java
+++ springMVCtestdemo/src/main/java/com/example/demo/controller/RegisterController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.demo.model.User;
 
 @Controller
 public class RegisterController {
@@ -10,6 +14,18 @@ public class RegisterController {
 	public String getRegisterForm() {
 		return "Register";
 	}
+	
+	@PostMapping("/register")
+	public String postSignUp(@ModelAttribute User user, Model model ) {
+		model.addAttribute("fname",user.getFname());
+		model.addAttribute("lname",user.getLname());
+		model.addAttribute("uname",user.getUsername());
+		model.addAttribute("psw",user.getPassword());
+		
+		return "Profile";
+	}
 
 	
+
 }
+
