cda68480079caaf3a5f1ab2a26c7defa5e8777a5
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-19 21:56:24 +0545

    login controller

--- springMVCtestdemo/src/main/java/com/example/demo/controller/LoginController.java
+++ springMVCtestdemo/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,13 +1,30 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.User;
 
 @Controller
 public class LoginController {
 	
 	@GetMapping("/login")
 	public String getLoginForm() {
+		return "Login";
+	}
+	
+	@PostMapping("/login")
+	public String postLogin(@ModelAttribute User user, Model model ) {
+		
+		if(user.getUsername().equals("ram")&& user.getPassword().equals("123")) {
+			model.addAttribute("uname",user.getUsername());
+			return "Home";
+		}
+		
+		model.addAttribute("message","user not found!!");
 		return "Login";
 	}
 }
