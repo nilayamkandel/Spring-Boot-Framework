d398ddb2085604328bf15fb3998cf99be09d913e
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-19 21:55:56 +0545

    user:model

---
+++ springMVCtestdemo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,40 @@
+package com.example.demo.model;
+
+public class User {
+	private int id;
+	private String fname;
+	private String lname;
+	private String username;
+	private String password;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
